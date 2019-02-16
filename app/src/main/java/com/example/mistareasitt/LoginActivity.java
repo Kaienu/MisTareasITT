1243d80d91ebf94846dbe05d16efc360c1434cd0
Kaienu <kaienu.jp@gmail.com>
2019-02-16 18:46:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mistareasitt/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.mistareasitt;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+    }
+
+    public void crearCuenta(View view){
+        Toast toast = Toast.makeText(this, "Función en desarrollo", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void botonLogin(View view){
+        TextInputEditText usuario = (TextInputEditText) findViewById(R.id.usuarioIn);
+        TextInputEditText passw = (TextInputEditText) findViewById(R.id.contraIn);
+
+        if(usuario.getText().toString().equalsIgnoreCase("itt") && passw.getText().toString().equalsIgnoreCase("1234") ){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast toast = Toast.makeText(this, "Credenciales inválidas", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+}
